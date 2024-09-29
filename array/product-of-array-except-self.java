8430ef89dd342c6310d72322325a4f2dbc1b5161
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-09-28 19:14:15 -0500

    LC-product-of-array-except-self [Runtime: N/A; Memory: N/A]

---
+++ array/product-of-array-except-self.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i= 0; i < nums.length; i++){
+            int temp =1;
+            for(int j = 0; j< nums.length;j++){
+                if(j!= i)temp = temp*nums[j];
+            }
+            res[i]=temp;
+        }
+        return res;
+    }
+}
