fc14830e05943b68fb4101283023fb9a845283d0
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-10-19 17:56:41 -0500

    LC-contains-duplicate-ii [Runtime: 22 ms; Memory: 57.6 MB]

--- hash-table/contains-duplicate-ii.java
+++ hash-table/contains-duplicate-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        for(int i = 0; i<nums.length-k; i++){
-            for (int j = 1; j<= k; j++){
-                if(nums[i+j]== nums[i]){
-                    return true;
-                }
-            }
+        HashMap <Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(nums[i])&&(i-map.get(nums[i]))<=k){
+                return true;
+            }else map.put(nums[i],i);
         }
         return false;
     }
