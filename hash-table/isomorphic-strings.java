531f86910905cfc4fa9e3ee56f207fdee6da0b67
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-10-25 22:48:47 -0500

    LC-isomorphic-strings [Runtime: N/A; Memory: N/A]

---
+++ hash-table/isomorphic-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> tmap = new HashMap<>();
+        for(int i = 0; i< s.length;i++){
+            if(!smap.containsKey(s.charAt(i))){
+                smap.put(s.charAt(i),i);
+            }
+            if(!tmap.containsKey(t.charAt(i))){
+                tmap.put(t.charAt(i),i);
+            }
+            if(tmap.get(t.charAt(i).equals(smap.get(s.charAt(i))))) return false;
+
+        }
+        return true;
+    }
+}
