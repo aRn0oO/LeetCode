73c2c04660a813bef459d69dbb18ab596fb043b3
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-11-02 12:19:07 -0500

    LC-rotate-array [Runtime: 0 ms; Memory: 57.4 MB]

--- array/rotate-array.java
+++ array/rotate-array.java
@@ -1,15 +1,18 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        int[] rotated = new int[n];
+        k %= nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
 
-        for (int i = 0; i < n; i++) {
-            rotated[(i + k) % n] = nums[i];
+    private void reverse(int[] nums, int l, int r){
+        while(l<r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
         }
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = rotated[i];
-        }        
     }
 }
