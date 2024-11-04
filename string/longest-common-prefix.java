f826de9712cb11ceb50af7ddd1582324bd5aff35
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-11-03 20:07:57 -0600

    LC-longest-common-prefix [Runtime: 1 ms; Memory: 41.3 MB]

--- string/longest-common-prefix.java
+++ string/longest-common-prefix.java
@@ -6,7 +6,7 @@ class Solution {
         String last = strs[strs.length-1];
         for(int i = 0; i < Math.min(first.length(), last.length());i++){
             if(first.charAt(i)!=last.charAt(i)){
-                return ans.toString();
+                break;
             }
             ans.append(first.charAt(i));
         }
