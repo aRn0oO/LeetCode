5717b7a88b8371b9a8fe3d3f38e831d368466008
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-11-04 20:54:52 -0600

    LC-h-index [Runtime: 2 ms; Memory: 41.2 MB]

---
+++ counting-sort/h-index.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int res= 0;
+        int n = citations.length;
+        for(int i = 0; i<n;i++){
+            if (citations[i]>= (n-i)) res = Math.max(n-i, res); 
+        }
+        return res;
+    }
+}
