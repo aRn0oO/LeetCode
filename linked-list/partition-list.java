6c3a32cd5404dea681de061a4ccc59867859d91c
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-11-19 20:31:25 -0600

    LC-partition-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/partition-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy; 
+        ListNode sec = new ListNode(0);
+        ListNode secTail = sec;
+        while(head!= null){
+            if(head.val< x){
+                tail.next = head;
+                tail = tail.next;
+            }else{
+                secTail.next = head;
+                secTail = secTail.next;
+            }
+            head = head.next;
+        }
+        sec.next = null;// terminate the greater list
+        tail.next = sec.next;
+
+        return dummy.next;
+    }
+}
