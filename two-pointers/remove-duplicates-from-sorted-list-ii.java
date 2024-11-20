3d7dc91cae29b4292e82077b47ecf5e69edde9ff
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-11-19 19:26:38 -0600

    LC-remove-duplicates-from-sorted-list-ii [Runtime: 0 ms; Memory: 42.9 MB]

--- two-pointers/remove-duplicates-from-sorted-list-ii.java
+++ two-pointers/remove-duplicates-from-sorted-list-ii.java
@@ -11,14 +11,13 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode dummy = new ListNode(0,head);
-        dummy.next = head;
-        ListNode prev = dummy.next;
-        ListNode curr = dummy.next.next;
-        while(curr != null){
-            if(curr.val == prev.val){
-                prev.next = curr.next;
-            }else prev = curr;
-            curr = curr.next;
+        ListNode prev = dummy;
+        while(head != null){
+            if(head.next != null && head.val== head.next.val){
+                while(head.next != null && head.val== head.next.val)head = head.next;
+                prev.next = head.next;
+            }else prev = prev.next;
+            head = head.next;
         }
         return dummy.next;
     }
