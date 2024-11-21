636f465d4bfacb6caa41fe15033ef330d67e04ef
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-11-21 11:19:59 -0600

    LC-same-tree [Runtime: 0 ms; Memory: 41.5 MB]

--- tree/same-tree.java
+++ tree/same-tree.java
@@ -15,8 +15,8 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&&q == null) return true;
-        if(p==null|| q == null||q.val != p.val)return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+        if(p==null&&q == null)return true;
+        if(p == null|| q == null|| q.val!= p.val) return false;
+        return isSameTree(p.right,q.right) && isSameTree(p.left, q.left);
     }
 }
