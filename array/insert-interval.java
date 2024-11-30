b71c0bfd8e15500761f0bc9ba1bd7ceba58d0802
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-11-30 13:58:26 -0600

    LC-insert-interval [Runtime: 1 ms; Memory: 45.4 MB]

--- array/insert-interval.java
+++ array/insert-interval.java
@@ -2,9 +2,9 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res = new ArrayList<>();
         int i = 0;
-        while(intervals[i][1]< newInterval[0]) res.add(intervals[i++]);
+        while(i< intervals.length&&intervals[i][1]< newInterval[0]) res.add(intervals[i++]);
 
-        while(i<intervals.length && intervals[i][0]<= newInterval[1]){
+        while(i<intervals.length && intervals[i][0]<= newInterval[1]){//<=
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
