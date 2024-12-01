70bbc149cdc41e3354f57fb49ef012a5d9723f70
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-01 14:21:09 -0600

    LC-letter-combinations-of-a-phone-number [Runtime: 5 ms; Memory: 42.3 MB]

---
+++ hash-table/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    String[] phone_map = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return Collections.emptyList();
+
+        
+        List<String> output = new ArrayList<>();
+        backtrack("", digits, output);
+        return output;
+    }
+
+    private void backtrack(String combination, String next_digits, List<String> output) {
+        if (next_digits.isEmpty()) {
+            output.add(combination);
+        } else {
+            String letters = phone_map[next_digits.charAt(0) - '2'];
+            for (char letter : letters.toCharArray()) {
+                backtrack(combination + letter, next_digits.substring(1), output);
+            }
+        }
+    }
+}
