e2e800111df24e767a86d411b0dfba888406cc8c
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-05 11:29:37 -0600

    LC-kth-smallest-element-in-a-bst [Runtime: 0 ms; Memory: 45 MB]

--- depth-first-search/kth-smallest-element-in-a-bst.java
+++ depth-first-search/kth-smallest-element-in-a-bst.java
@@ -24,13 +24,13 @@ class Solution {
     }
 
     void helper(TreeNode root){
-        if(root.left != null) root = root.left;
+        if(root.left != null) helper(root.left);
         count --;
         if(count == 0){
             num = root.val;
             return;
         }
-        if(root.right != null) root = root.right;
+        if(root.right != null) helper(root.right);
 
     }
 
