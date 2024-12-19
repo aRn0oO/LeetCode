25a7a577a622fbe1ebabc0882433a6a20483c292
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-19 07:33:45 -0600

    LC-search-a-2d-matrix [Runtime: 0 ms; Memory: 42.1 MB]

---
+++ array/search-a-2d-matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix[0].length, n = matrix .length;
+        int l = 0, r = m*n - 1;
+
+        while (l<= r){
+            int mid = l + (r-l)/2;
+            int mid_val = matrix[mid/m][mid%m];
+
+            if( mid_val == target) return true;
+            else if( mid_val < target) l = mid +1;
+            else r = mid -1;
+        }
+
+        return false;
+
+
+    }
+}
