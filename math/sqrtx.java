1fa0566a0dcefa130af7e8deeb0948217dc0701b
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-22 11:50:31 -0600

    LC-sqrtx [Runtime: 1 ms; Memory: 40.7 MB]

--- math/sqrtx.java
+++ math/sqrtx.java
@@ -2,7 +2,7 @@ class Solution {
     public int mySqrt(int x) {
         if(x== 0|| x==1) return x;
         int r = x/2, l = 0;
-        while(l< r){
+        while(l<=r){
             int mid = l+(r-l)/2;
             if(mid*mid == x) return mid;
             else if ((long) mid*mid > (long)x) r = mid -1;
