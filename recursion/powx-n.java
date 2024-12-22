4abb75244e81bf95052f106ef87e7932a60806d2
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-22 17:56:31 -0600

    LC-powx-n [Runtime: 0 ms; Memory: 41.9 MB]

--- recursion/powx-n.java
+++ recursion/powx-n.java
@@ -1,11 +1,10 @@
-public class Solution {
-    public double pow(double x, int n) {
+class Solution {
+    public double myPow(double x, int n) {
         if(n == 0)
             return 1;
         if(n<0){
-            n = -n;
-            x = 1/x;
+            return 1/x * myPow(1/x, -(n + 1));
         }
-        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
     }
 }
