954d6867e62ae025923e80b5624a71061a86a8b5
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-22 18:05:33 -0600

    LC-climbing-stairs [Runtime: 0 ms; Memory: 40.8 MB]

--- math/climbing-stairs.java
+++ math/climbing-stairs.java
@@ -1,6 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
         if(n == 0||n==1) return 1;
-        return climbStairs(n-1)+climbStairs(n-2);
+        int prev = 1, cur = 1;
+        for (int i = 2; i<= n;i++){
+            int temp = cur;
+            cur += prev;
+            prev= temp;
+        }
+        return cur;
     }
 }
