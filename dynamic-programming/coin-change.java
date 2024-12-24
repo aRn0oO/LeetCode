f91889937c6155451c9a88108d1485ba7c5fd9c1
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-23 21:44:35 -0600

    LC-coin-change [Runtime: 15 ms; Memory: 44.5 MB]

--- dynamic-programming/coin-change.java
+++ dynamic-programming/coin-change.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        if(amount< 0) return -1;
-        if( amount == 0) return 0;
-        int res = -1;
-        for(int i = 0; i<coins.length; i++){
-            int coin = coinChange( coins, amount- coins[i]);
-            if(coin>= 0) res = res<0? coin +1:Math.min(res,coin+1 );
-        }
-        return res;
+	public int coinChange(int[] coins, int amount) {
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+            for(int c:coins)
+                if(i>=c) dp[i]=Math.min(dp[i],dp[i-c]+1);
+        return dp[amount]>amount?-1:dp[amount];
     }
 }
