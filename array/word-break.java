55503d64421ae9db031ceb37ef992cc68c63bc8e
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-25 10:45:57 -0600

    LC-word-break [Runtime: N/A; Memory: N/A]

---
+++ array/word-break.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreak(0,s, wordDict);
+
+    }
+
+    boolean wordBreak(int p, String s, List<String> wordDict){
+        int n = s.length();
+        if(p == n) return true;
+        for(int i = p+1; i <= n; i ++){
+            if(wordDict.contains(s.substring(p,i))&&wordBreak(i,s, wordDict)) return true;
+        }
+        return false;
+    }
+}
