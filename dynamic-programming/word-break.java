2bf46eba2969104af000df8b7d5b5bd7e396526d
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2024-12-25 10:48:13 -0600

    LC-word-break [Runtime: N/A; Memory: N/A]

--- dynamic-programming/word-break.java
+++ dynamic-programming/word-break.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreak(0,s, wordDict);
+        return wordBreak(0,s, new HashSet(wordDict));
 
     }
 
-    boolean wordBreak(int p, String s, List<String> wordDict){
+    boolean wordBreak(int p, String s, Set<String> wordDict){
         int n = s.length();
         if(p == n) return true;
         for(int i = p+1; i <= n; i ++){
