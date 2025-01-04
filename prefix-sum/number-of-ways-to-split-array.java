4913350ad98cf3096e8ec4b1c4588f838ee27b3b
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-01-04 14:26:17 -0600

    LC-number-of-ways-to-split-array [Runtime: N/A; Memory: N/A]

--- prefix-sum/number-of-ways-to-split-array.java
+++ prefix-sum/number-of-ways-to-split-array.java
@@ -2,15 +2,13 @@ class Solution {
     public int waysToSplitArray(int[] nums) {
         int count = 0;
         int n = nums.length;
+        int l = 0, r = 0;
+        for(int i : nums) r+=i;
         for(int i = 0; i < n-1;i++){
-            int l = 0, r = 0;
-            for(int j = 0; j<= i; j ++){
-                l += nums[j];
-            }
-            for(int j= i+1; j < n;j++){
-                r += nums[j];
-            }
-            if (l>= r) count ++;
+            l+= nums[i];
+            r-= nums[i];
+
+            if(l>=r) count ++;
         }
         return count;
     }
