c27cb3429d7ca2bd88519ae0e6df9051839253e8
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-01-05 13:35:11 -0600

    LC-unique-length-3-palindromic-subsequences [Runtime: 170 ms; Memory: 45.6 MB]

---
+++ string/unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int []first = new int[26];
+        int []last  = new int[26];
+        
+        Arrays.fill(first, Integer.MAX_VALUE);
+
+        for(int i = 0; i < s.length();i++){
+            char ch = s.charAt(i);
+            first[ch-'a'] = Math.min(first[ch-'a'], i);
+            last[ch-'a'] = i;
+        }
+
+        int res = 0; 
+        HashSet <Character> set = new HashSet<>();
+        for(int i = 0; i < 26; i++){
+            if( first[i]!=Integer.MAX_VALUE && (last[i]-first[i])>1){
+                set.clear();
+                for( int j = first[i]+1; j<last[i]; j++) set.add(s.charAt(j));
+                res += set.size();
+            }
+        }
+        return res;
+    }
+}
