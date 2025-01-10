f57e2a845635da858a053572de280675d3d76a44
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-01-10 17:45:30 -0600

    LC-word-subsets [Runtime: 13 ms; Memory: 54.6 MB]

--- array/word-subsets.java
+++ array/word-subsets.java
@@ -1,13 +1,27 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        List<String> res = new ArrayList<String>();
+        List<String> res = new ArrayList<>();
+        int[] target = new int[26];
+        for(String word: words2){
+            int[]temp = new int[26];
+            for( char ch : word.toCharArray()){
+                temp[ch-'a']++;
+                target[ch-'a'] = Math.max(target[ch-'a'], temp[ch-'a']);
+            }
+        }
         for(String word: words1){
-            if(helper(word, 0, words2)) res.add(word);
+            int[] source = new int[26];
+            for(char ch : word.toCharArray()) source[ch-'a']++;
+
+            if(helper(source, target)) res.add(word);
+
         }
         return res;
     }
-    boolean helper(String word, int index, String[] words2){
-        if(index>= words2.length) return true;
-        return word.contains(words2[index])&&helper(word, index+1, words2);
+    boolean helper(int[] parent, int [] child){
+        for(int i=0;i<26;i++){
+            if(parent[i]<child[i]) return false;
+        }
+        return true;
     }
 }
