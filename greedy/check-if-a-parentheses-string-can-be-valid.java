e89cdb4cdb8b58a79b32f14f1cd464b2d8342266
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-01-12 10:55:11 -0600

    LC-check-if-a-parentheses-string-can-be-valid [Runtime: N/A; Memory: N/A]

--- greedy/check-if-a-parentheses-string-can-be-valid.java
+++ greedy/check-if-a-parentheses-string-can-be-valid.java
@@ -3,27 +3,22 @@ class Solution {
         int n = s.length();
         if(n%2==1) return false;
 
-        Stack<Integer> open = new Stack<>();
-        Stack<Integer> unlocked = new Stack<>();
-
-        for( int i = 0; i < n; i++){
-            if(locked.charAt(i) =='0'){
-                unlocked.push(i);
-            }else if(s.charAt(i)=='(') open.push(i);
-            else{
-                if(!open.isEmpty()) open.pop();
-                else if(!unlocked.isEmpty()) unlocked.pop();
-                else return false;
-            }
+        int open = 0;
+        for(int i = 0; i< n; i++){
+            if(s.charAt(i)=='('|| locked.charAt(i)=='0') open++;
+            else open--;
+            if(open<0) return false;//
+            
         }
-
-        while(!open.isEmpty()&& !unlocked.isEmpty()&& open.peek()<unlocked.peek() ){
-            open.pop();
-            unlocked.pop();
-        }
-
-        if(open.isEmpty() && !unlocked.isEmpty()) return unlocked.size()%2==0;
         
-        return open.isEmpty();
+        int close = 0;
+        for(int i = n-1; i>=0;i--) {
+            if(s.charAt(i)=='('||locked.charAt(i)=='0') close++;
+            else close --;
+            if(close< 0)return false;//
+        }
+        
+
+        return true;
     }
 }
