835f861bf0922c32e5f730948e979d94858f18dd
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-01-17 15:09:42 -0600

    LC-neighboring-bitwise-xor [Runtime: 2 ms; Memory: 62.3 MB]

--- bit-manipulation/neighboring-bitwise-xor.java
+++ bit-manipulation/neighboring-bitwise-xor.java
@@ -1,5 +1,11 @@
 class Solution {
-    public boolean doesValidArrayExist(int[] A) {
-        return Arrays.stream(A).sum() % 2 == 0;
+    public boolean doesValidArrayExist(int[] derived) {
+        int XOR = 0;
+
+        for(int x : derived) {
+            XOR = (XOR ^ x);
+        }
+
+        return XOR == 0;
     }
-} 
+}
