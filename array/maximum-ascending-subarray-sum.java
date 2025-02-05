1d946b61c70cb22a3bcb0f914d4a16c216587abb
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-02-05 12:39:01 -0600

    LC-maximum-ascending-subarray-sum [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-ascending-subarray-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int sum = nums[0], max = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] < nums[i]) sum+=nums[i];
+            else {
+                
+                sum =nums[i];
+            }
+            max = Math.max (sum, max);
+        }
+        return max;
+    }
+}
