b539e60f78aac108c28bba7b23f71f0d39e731ad
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-02-06 14:43:17 -0600

    LC-max-number-of-k-sum-pairs [Runtime: 35 ms; Memory: 55.6 MB]

--- two-pointers/max-number-of-k-sum-pairs.java
+++ two-pointers/max-number-of-k-sum-pairs.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
-
-        int i = 0, j = nums.length-1;
-        while(i<j ){
-            int sum = nums[i]+nums[j];
-            if (sum> k) j --;
-            else if(sum< k) i++;
-            else{
+        for(int i = 0; i < nums.length; i++){
+            int res = k - nums[i];
+            if(map.containsKey(res)){
                 count++;
-                i++;
-                j--;
+                if(map.get(res) == 1) map.remove(res);
+                else map.put(res, map.get(res) - 1);
+            }
+            else{
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
         }
-        return count; 
+        return count;
     }
 }
