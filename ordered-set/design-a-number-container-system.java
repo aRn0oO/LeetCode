0be06fbaea914781faaf74b5328d91c6646ab12f
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-02-08 11:01:41 -0600

    LC-design-a-number-container-system [Runtime: 69 ms; Memory: 159.7 MB]

--- ordered-set/design-a-number-container-system.java
+++ ordered-set/design-a-number-container-system.java
@@ -3,25 +3,40 @@ import java.util.*;
 class NumberContainers {
     private Map<Integer, PriorityQueue<Integer>> res;
     private Map<Integer, Integer> index_val;
+    private Map<Integer, Set<Integer>> index_sets;
 
     public NumberContainers() {
         res = new HashMap<>();
         index_val = new HashMap<>();
+        index_sets = new HashMap<>();
     }
     
     public void change(int index, int number) {
         if (index_val.containsKey(index)) {
             int prevNum = index_val.get(index);
-            if (prevNum == number) return;
-            res.get(prevNum).remove(index);
+            if (prevNum == number) return; // No change needed
+            
+            // Efficient removal using HashSet
+            index_sets.get(prevNum).remove(index);
+            if (index_sets.get(prevNum).isEmpty()) {
+                res.remove(prevNum); // Cleanup if empty
+                index_sets.remove(prevNum);
+            }
         }
 
+        // Add index to new number
         res.computeIfAbsent(number, k -> new PriorityQueue<>()).offer(index);
+        index_sets.computeIfAbsent(number, k -> new HashSet<>()).add(index);
         index_val.put(index, number);
     }
     
     public int find(int number) {
-        PriorityQueue<Integer> pq = res.getOrDefault(number, new PriorityQueue<>());
+        if (!res.containsKey(number)) return -1;
+
+        PriorityQueue<Integer> pq = res.get(number);
+        while (!pq.isEmpty() && !index_sets.get(number).contains(pq.peek())) {
+            pq.poll(); // Remove outdated entries
+        }
         return pq.isEmpty() ? -1 : pq.peek();
     }
 }
