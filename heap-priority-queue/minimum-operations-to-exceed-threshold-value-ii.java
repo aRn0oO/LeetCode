19098e9f6c6a3d7211a50f68577ea348ec697d7c
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-02-13 11:07:55 -0600

    LC-minimum-operations-to-exceed-threshold-value-ii [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/minimum-operations-to-exceed-threshold-value-ii.java
+++ heap-priority-queue/minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,7 +1,19 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int count = 0;
-        for (int num : nums) {if(num<k) count++;}
-        return count-1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            if (num < k) pq.add(num);
+        }
+        int count = 0; 
+        while(!pq.isEmpty()){
+            int x = pq.poll();
+            count++;
+            if(pq.isEmpty()) return count; 
+            int y = pq.poll();
+            int newVal = 2*x+y;
+            if(newVal<k)pq.add(newVal);
+
+        }
+        return count;
     }
 }
