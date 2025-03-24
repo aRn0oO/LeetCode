205cd1751d1e50defce4117f193529418ce3cb0c
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-03-24 16:38:34 -0500

    LC-count-days-without-meetings [Runtime: 45 ms; Memory: 98.9 MB]

--- array/count-days-without-meetings.java
+++ array/count-days-without-meetings.java
@@ -1,15 +1,21 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        int[] day = new int[days+1];
-        for(int []meeting : meetings){
-            for (int i = meeting[0]; i<=meeting[1];i++){
-                day[i]+= 1;
+        Arrays.sort(meetings, Comparator.comparingInt(a->a[0]));
+        
+        ArrayList<int[]> list = new ArrayList<>();
+
+        for(int[]meeting: meetings){
+            if(list.isEmpty()|| meeting[0]> list.get(list.size()-1)[1]) list.add(meeting);
+            else{
+                list.get(list.size()-1)[1] = Math.max(list.get (list.size()-1)[1],meeting[1]);
             }
         }
-        int res = 0;
-        for(int i: day){
-            if(i==0) res++;
+        int meetingDaysCount = 0;
+        for (int[] m : list) {
+            meetingDaysCount += (m[1] - m[0] + 1);
         }
-        return res-1;
+
+        return days - meetingDaysCount;
+
     }
 }
