b1b65fe224d6cd9c4983818cbaf2e39351432826
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-07-29 18:17:59 -0500

    LC-smallest-subarrays-with-maximum-bitwise-or [Runtime: 26 ms; Memory: 61.3 MB]

--- bit-manipulation/smallest-subarrays-with-maximum-bitwise-or.java
+++ bit-manipulation/smallest-subarrays-with-maximum-bitwise-or.java
@@ -1,31 +1,14 @@
 class Solution {
-    public int[] smallestSubarrays(int[] nums) {
-        int n = nums.length;
-        int [] lengthList = new int[n];
-             
-
-        for(int i = 0; i< n; i++){
-            int max = 0;
-            for (int k = i; k < n; k++) {
-                max |= nums[k];
+        public int[] smallestSubarrays(int[] A) {
+        int n = A.length, last[] = new int[30], res[] = new int[n];
+        for (int i = n - 1; i >= 0; --i) {
+            res[i] = 1;
+            for (int j = 0; j < 30; ++j) {
+                if ((A[i] & (1 << j)) > 0)
+                    last[j] = i;
+                res[i] = Math.max(res[i], last[j] - i + 1);
             }
-
-            int curOr = 0;
-            for (int j = i; j<n; j++){
-                
-                curOr = curOr | nums[j];
-                if(curOr == max) {
-                    lengthList[i] = j-i+1;
-                    break;
-                }    
-            }
-
         }
-        return lengthList;
-
-
-
-
-
+        return res;
     }
 }
