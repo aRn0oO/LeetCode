952ccbf66b5f4083e16eeeb05bc3f9574a93374d
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-09 10:53:18 -0500

    LC-koko-eating-bananas [Runtime: N/A; Memory: N/A]

--- array/koko-eating-bananas.java
+++ array/koko-eating-bananas.java
@@ -1,22 +1,22 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        Arrays.sort(piles);
+        int left = 1;
+        int right = 1000000000;
         
-        int eat = -1;
-        int l = 1, r = piles[piles.length-1];
-    
-        while(l<r){
-            eat = l+r/2;
-            int currhour = 0;
-            for(int i : piles)
-                currhour+= (i/eat) + (i%eat ==0? 0:1);
-            
-            
-            if(currhour > h)   
-                l = eat+1;
-            else r = eat-1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(canEatInTime(piles, mid, h)) right = mid - 1;
+            else left = mid + 1;
         }
-
-        return eat;
+        return left;
+    }
+    public boolean canEatInTime(int piles[], int k, int h){
+        int hours = 0;
+        for(int pile : piles){
+            int div = pile / k;
+            hours += div;
+            if(pile % k != 0) hours++;
+        }
+        return hours <= h;
     }
 }
