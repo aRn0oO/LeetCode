4588d2bb83afd454c32d9237c5742c5d024bde91
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-09 10:54:04 -0500

    LC-koko-eating-bananas [Runtime: 28 ms; Memory: 45.5 MB]

--- binary-search/koko-eating-bananas.java
+++ binary-search/koko-eating-bananas.java
@@ -1,22 +1,16 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;
-        int right = 1000000000;
-        
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(canEatInTime(piles, mid, h)) right = mid - 1;
-            else left = mid + 1;
+    public int minEatingSpeed(int[] piles, int H) {
+        int low = 1, high = 1000000000, k = 0;
+        while (low <= high) {
+            k = (low + high) / 2;
+            int h = 0;
+            for (int i = 0; i < piles.length; i ++) 
+                h += Math.ceil(1.0 * piles[i] / k);
+            if (h > H)
+                low = k + 1;
+            else
+                high = k - 1;
         }
-        return left;
-    }
-    public boolean canEatInTime(int piles[], int k, int h){
-        int hours = 0;
-        for(int pile : piles){
-            int div = pile / k;
-            hours += div;
-            if(pile % k != 0) hours++;
-        }
-        return hours <= h;
+        return low;
     }
 }
