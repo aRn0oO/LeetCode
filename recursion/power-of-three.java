0aa7b409324c86e6ec1558b34ab0ee0563d6577c
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-13 12:00:02 -0500

    LC-power-of-three [Runtime: N/A; Memory: N/A]

---
+++ recursion/power-of-three.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n == 3) return true;
+        if(n <3) return false;
+        return n%3 == 0 && isPowerOfThree(n/3);
+    }
+}
