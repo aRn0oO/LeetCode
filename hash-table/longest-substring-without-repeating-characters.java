2dc0995d7e89787e85a1117998c955c4e85c2620
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-14 16:44:17 -0500

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-without-repeating-characters.java
+++ hash-table/longest-substring-without-repeating-characters.java
@@ -1,15 +1,15 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+        if(s.length()<2) return s.length();
         int maxLength = 0;
 
         for(int i = 0; i < s.length(); i++){
-            int [] al = new int[26];
+            int [] al = new int[256];
             int curLength = 0;
             for(int j = i; j< s.length();j++){
-                int index = s.charAt(j)-'a';
-                if(al[index]==0){
-                    al[index]++;
+                
+                if(al[s.charAt(j)]==0){
+                    al[s.charAt(j)]++;
                     curLength++;
                 }
                 else {
