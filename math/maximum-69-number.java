bab76affcb3c08312a19c934a7a0c201cc8f96ca
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-16 13:20:46 -0500

    LC-maximum-69-number [Runtime: 0 ms; Memory: 40.6 MB]

--- math/maximum-69-number.java
+++ math/maximum-69-number.java
@@ -1,5 +1,14 @@
 class Solution {
     public int maximum69Number (int num) {
-        return Integer.parseInt(("" + num).replaceFirst("6", "9"));
+       int add= 0, step = 3, temp = num;
+       while(temp> 0){
+        int d = temp%10;
+        temp /=10;
+
+        if(d == 6) add = step;
+        step *= 10;
+       }
+       return num + add;
+
     }
 }
