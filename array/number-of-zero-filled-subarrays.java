2a2c5dc2f353117aad4eca5d8f8a02c62f236333
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-20 12:54:15 -0500

    LC-number-of-zero-filled-subarrays [Runtime: 3 ms; Memory: 61.7 MB]

--- array/number-of-zero-filled-subarrays.java
+++ array/number-of-zero-filled-subarrays.java
@@ -1,6 +1,6 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        int  res = 0, streak = 0;
+        long  res = 0, streak = 0;
 
         for(int num: nums){
             streak = (num ==0)? streak +1: 0;
