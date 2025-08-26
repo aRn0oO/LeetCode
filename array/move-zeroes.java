427d399d2cc098f369875bdad4f1d167f64f16c6
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-26 14:03:35 -0500

    LC-move-zeroes [Runtime: 2 ms; Memory: 46.2 MB]

--- array/move-zeroes.java
+++ array/move-zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int l = 0;
-        for( int r = 0; r< nums.length; r++){
-            if(nums[r] != 0){
-                int temp = nums[r];
-                nums[r] = nums[l];
-                nums[l] = temp;
-                l++;
+       int point = 0;                  // number of zeros seen so far
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                point++;                // accumulate zeros
+            } else if (point != 0) {    // we’ve seen 'point' zeros before this i
+                nums[i - point] = nums[i]; // shift current non-zero left by 'point'
+                nums[i] = 0;               // leave a zero in current position
             }
         }
     
