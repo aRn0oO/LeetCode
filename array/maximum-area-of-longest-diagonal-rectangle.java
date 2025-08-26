edd2aef9a0d4351aee21ff8147185b5976228809
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-26 12:43:08 -0500

    LC-maximum-area-of-longest-diagonal-rectangle [Runtime: N/A; Memory: N/A]

--- array/maximum-area-of-longest-diagonal-rectangle.java
+++ array/maximum-area-of-longest-diagonal-rectangle.java
@@ -1,9 +1,9 @@
 class Solution {
     public int areaOfMaxDiagonal(int[][] dimensions) {
-        int[] pair = dimensions[0];
+  
         int max = 0, res = 0;
 
-        for(int i = 1; i < dimensions.length; i++ ){
+        for(int i = 0; i < dimensions.length; i++ ){
             int l = dimensions[i][0], w = dimensions[i][1];
             if((l*l+w*w)>max) {
                 max = l*l+w*w;
