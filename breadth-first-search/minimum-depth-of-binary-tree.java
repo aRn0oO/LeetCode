0490d6597d9f0344711bc8fdf420d994c05dbed9
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-08-27 12:22:06 -0500

    LC-minimum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/minimum-depth-of-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    public int minDepth(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        dfs(root, 1);
+        return ans;
+    }
+    void dfs(TreeNode root, int level ){
+        if(root == null) return;
+
+        if(root.left== null && root.right==null)ans = Math.min(ans, level);
+
+        dfs(root.left, level+1);
+        dfs(root.right, level+1);
+    }
+
+
+}
