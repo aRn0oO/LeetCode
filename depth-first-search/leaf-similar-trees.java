212deec6b657834f77a84ea3e8149e73afe84152
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-09-02 16:44:26 -0500

    LC-leaf-similar-trees [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/leaf-similar-trees.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer>list1 = new ArrayList<>(), list2 = new ArrayList<>();
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        helper(root1, list1);
+        helper(root2, list2); 
+        if(list1.size()!=list2.size())return false;
+        int n = list1.size();
+        for(int i = 0; i<n;i++){
+            if(list1.get(i)!= list2.get(i)) return false;
+        }
+        return true;
+    }
+
+    void helper(TreeNode root,List<Integer> list){
+        if(root.left == null && root.right == null){
+            list.add(root.val);
+            return;
+
+        }   
+
+        helper(root.left,list);
+        helper(root.right, list);  
+    }
+}
