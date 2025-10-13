6be121a5de41d3e437fc74e39a14ccd357371000
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-10-13 11:34:44 -0500

    LC-find-resultant-array-after-removing-anagrams [Runtime: 2 ms; Memory: 44.7 MB]

---
+++ string/find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> list = new ArrayList<>();
+
+        String prev = "";
+        for(int i = 0;i<words.length;i++){
+            char[] ch = words[i].toCharArray();
+            Arrays.sort(ch);
+
+            String curr = String.valueOf(ch);
+
+            if(!curr.equals(prev)){
+                list.add(words[i]);
+
+                prev = curr;
+            }
+
+        }
+        return list;
+    }
+}
