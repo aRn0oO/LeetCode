cae9af21b117a789577977821a1499ed38b2ece3
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-10-17 15:01:09 -0500

    LC-4sum [Runtime: N/A; Memory: N/A]

--- sorting/4sum.java
+++ sorting/4sum.java
@@ -1,54 +1,31 @@
-    public class Solution {
-        int len = 0;
-        public List<List<Integer>> fourSum(int[] nums, int target) {
-            len = nums.length;
-            Arrays.sort(nums);
-            return kSum(nums, target, 4, 0);
-        }
-       private ArrayList<List<Integer>> kSum(int[] nums, int target, int k, int index) {
-            ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
-            if(index >= len) {
-                return res;
-            }
-            if(k == 2) {
-            	int i = index, j = len - 1;
-            	while(i < j) {
-                    //find a pair
-            	    if(target - nums[i] == nums[j]) {
-            	    	List<Integer> temp = new ArrayList<>();
-                    	temp.add(nums[i]);
-                    	temp.add(target-nums[i]);
-                        res.add(temp);
-                        //skip duplication
-                        while(i<j && nums[i]==nums[i+1]) i++;
-                        while(i<j && nums[j-1]==nums[j]) j--;
-                        i++;
-                        j--;
-                    //move left bound
-            	    } else if (target - nums[i] > nums[j]) {
-            	        i++;
-                    //move right bound
-            	    } else {
-            	        j--;
-            	    }
-            	}
-            } else{
-                for (int i = index; i < len - k + 1; i++) {
-                    //use current number to reduce ksum into k-1sum
-                    ArrayList<List<Integer>> temp = kSum(nums, target - nums[i], k-1, i+1);
-                    if(temp != null){
-                        //add previous results
-                        for (List<Integer> t : temp) {
-                            t.add(0, nums[i]);
-                        }
-                        res.addAll(temp);
-                    }
-                    while (i < len-1 && nums[i] == nums[i+1]) {
-                        //skip duplicated numbers
-                        i++;
-                    }
+public class Solution {
+public List<List<Integer>> fourSum(int[] num, int target) {
+    ArrayList<List<Integer>> ans = new ArrayList<>();
+    if(num.length<4)return ans;
+    Arrays.sort(num);
+    for(int i=0; i<num.length-3; i++){
+        if(num[i]+num[i+1]+num[i+2]+num[i+3]>target)break; //first candidate too large, search finished
+        if(num[i]+num[num.length-1]+num[num.length-2]+num[num.length-3]<target)continue; //first candidate too small
+        if(i>0&&num[i]==num[i-1])continue; //prevents duplicate result in ans list
+        for(int j=i+1; j<num.length-2; j++){
+            if(num[i]+num[j]+num[j+1]+num[j+2]>target)break; //second candidate too large
+            if(num[i]+num[j]+num[num.length-1]+num[num.length-2]<target)continue; //second candidate too small
+            if(j>i+1&&num[j]==num[j-1])continue; //prevents duplicate results in ans list
+            int low=j+1, high=num.length-1;
+            while(low<high){
+                int sum=num[i]+num[j]+num[low]+num[high];
+                if(sum==target){
+                    ans.add(Arrays.asList(num[i], num[j], num[low], num[high]));
+                    while(low<high&&num[low]==num[low+1])low++; //skipping over duplicate on low
+                    while(low<high&&num[high]==num[high-1])high--; //skipping over duplicate on high
+                    low++; 
+                    high--;
                 }
+                //move window
+                else if(sum<target)low++; 
+                else high--;
             }
-            return res;
         }
     }
+    return ans;
+}
