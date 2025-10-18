c9343ddad7ae8778909b4913e84756ba2beab107
aRn0oO <146026517+aRn0oO@users.noreply.github.com>
2025-10-18 11:54:42 -0500

    LC-4sum [Runtime: 19 ms; Memory: 45.7 MB]

--- two-pointers/4sum.java
+++ two-pointers/4sum.java
@@ -1,49 +1,38 @@
-import java.util.*;
-
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+   public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        return kSum(nums, 0, 4, (long) target);
+        return kSum(nums, 0, 4, (long)target);
     }
-
-    private List<List<Integer>> kSum(int[] nums, int start, int k, long target) {
-        int n = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
-        if (start >= n || k < 2) return res;
-
-        // --- PRUNING: compute min and max possible sum with k numbers ---
-        long min = 0L, max = 0L;
-        for (int i = 0; i < k; i++) {
-            if (start + i >= n || n - 1 - i < 0) return res; // safety
-            min += nums[start + i];
-            max += nums[n - 1 - i];
-        }
-        if (target < min || target > max) return res;
-
-        if (k == 2) {
-            int l = start, r = n - 1;
-            while (l < r) {
-                long sum = (long) nums[l] + nums[r];
-                if (sum < target) {
-                    l++;
-                } else if (sum > target) {
-                    r--;
-                } else {
-                    res.add(Arrays.asList(nums[l], nums[r]));
-                    int a = nums[l], b = nums[r];
-                    while (l < r && nums[l] == a) l++;  // skip dups
-                    while (l < r && nums[r] == b) r--;  // skip dups
+    private List<List<Integer>> kSum (int[] nums, int start, int k, long target) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(k == 2) { //two pointers from left and right
+            int left = start, right = len - 1;
+            while(left < right) {
+                int sum = nums[left] + nums[right];
+                if(sum == target) {
+                    List<Integer> path = new ArrayList<Integer>();
+                    path.add(nums[left]);
+                    path.add(nums[right]);
+                    res.add(path);
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < target) { //move left
+                    left++;
+                } else { //move right
+                    right--;
                 }
             }
-            return res;
-        }
-
-        for (int i = start; i <= n - k; i++) {
-            if (i > start && nums[i] == nums[i - 1]) continue; // dedupe
-            long newTarget = target - nums[i];
-            for (List<Integer> tail : kSum(nums, i + 1, k - 1, newTarget)) {
-                tail.add(0, nums[i]);
-                res.add(tail);
+        } else {
+            for(int i = start; i < len - (k - 1); i++) {
+                if(i > start && nums[i] == nums[i - 1]) continue;
+                List<List<Integer>> temp = kSum(nums, i + 1, k - 1, target - nums[i]);
+                for(List<Integer> t : temp) {
+                   t.add(0, nums[i]);
+                }                    
+                res.addAll(temp);
             }
         }
         return res;
